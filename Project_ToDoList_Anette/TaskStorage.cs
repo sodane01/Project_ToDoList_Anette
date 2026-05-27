a26b8bca1e072a777ba7b7cc6321ca55e52b79b0
Anette Lindgren Söderström <sodane01@gmail.com>
2026-05-27 16:54:31 +0200

    Städade i koden lite

--- Project_ToDoList_Anette/TaskStorage.cs
+++ Project_ToDoList_Anette/TaskStorage.cs
@@ -1,46 +1,4 @@
-﻿//public class TaskStorage
-//{
-//    private readonly string filePath = "tasks.csv";
-
-//    public List<TodoTask> LoadTasks()
-//    {
-//        List<TodoTask> tasks = new();
-
-//        if (!File.Exists(filePath))
-//        {
-//            return tasks;
-//        }
-
-//        var lines = File.ReadAllLines(filePath).Skip(1);
-
-//        foreach (var line in lines)
-//        {
-//            var parts = line.Split(',');
-
-//            int id = int.Parse(parts[0]);
-//            string title = parts[1];
-//            string project = parts[2];
-//            DateTime dueDate = DateTime.Parse(parts[3]);
-//            bool isDone = bool.Parse(parts[4]);
-
-//            tasks.Add(new TodoTask(id, title, project, dueDate, isDone));
-//        }
-
-//        return tasks;
-//    }
-
-//    public void SaveTasks(List<TodoTask> tasks)
-//    {
-//        using StreamWriter writer = new StreamWriter(filePath);
-
-//        writer.WriteLine("Id,Title,Project,DueDate,IsDone");
-
-//        foreach (var task in tasks)
-//        {
-//            writer.WriteLine($"{task.Id},{task.Title},{task.Project},{task.DueDate:yyyy-MM-dd},{task.IsDone}");
-//        }
-//    }
-//}
+﻿
 
 public class TaskStorage
 {
--- Project_ToDoList_Anette/TodoTask.cs
+++ Project_ToDoList_Anette/TodoTask.cs
@@ -1,28 +1,4 @@
-﻿//public class TodoTask
-//{
-//    public int Id { get; set; }
-
-//    public string Title { get; set; } = "";
-
-//    public string Project { get; set; } = "";
-
-//    public DateTime DueDate { get; set; }
-
-//    public bool IsDone { get; set; }
-
-//    public TodoTask()
-//    {
-//    }
-
-//    public TodoTask(int id, string title, string project, DateTime dueDate, bool isDone)
-//    {
-//        Id = id;
-//        Title = title;
-//        Project = project;
-//        DueDate = dueDate;
-//        IsDone = isDone;
-//    }
-//}
+﻿
 
 // Represents a single task in the todo list application
 public class TodoTask
